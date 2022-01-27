5dbe68f419b8ff65f7fd67941ce5f3ca13dcf71f
Logan Sritharan <ls718@ic.ac.uk>
2022-01-27 18:30:02 +0000

    cleaner image output

--- Plotter.jl
+++ Plotter.jl
@@ -3,7 +3,7 @@ p = plot()
 
 # plot circles
 for i in range(1,stop=length(circles))
-    plot!(Functions.draw(circles[i],0,2*pi), aspect_ratio=1, label="", color="black")
+    plot!(Functions.draw(circles[i],0,2*pi), aspect_ratio=1, label="", color="black", ylim=(0,domain_y), xlim=(0,domain_x), legend=:outertopright)
 end
 
 # plot intersection points, white = on boundary contour, red = inside boundary contour
