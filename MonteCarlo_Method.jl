68372d8a97d7bf2e0e1c6bd65f5b4774f20aa5c4
Logan Sritharan <ls718@ic.ac.uk>
2022-01-27 18:29:34 +0000

    intial commit

---
+++ MonteCarlo_Method.jl
@@ -0,0 +1,30 @@
+module MonteCarlo_Method
+import Functions
+
+function Area(circles,N_MC,domain)
+    N_MC = 100000
+    count = 0
+    domain_x,domain_y = domain
+    for i in range(1,stop=N_MC)
+        point_x = rand(1)[1]*(domain_x)
+        point_y = rand(1)[1]*(domain_y)
+
+        Point = Functions.Point(point_x,point_y,[],false)
+
+        for j in range(1,stop=size(circles)[1])
+            A = circles[j]
+            inside = Functions.boundary(A,Point)
+
+            if !inside #point inside a circle
+                count = count + 1
+                break
+            end
+        end
+
+    end
+
+    area = count/N_MC * (domain_x*domain_y)
+    println("Total Area (MonteCarlo): ",area)
+end
+
+end #module end
