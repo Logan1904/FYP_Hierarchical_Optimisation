f6520b0bd0a2354db8dd91cec4504108653e0f51
Logan Sritharan <ls718@ic.ac.uk>
2022-03-12 23:16:50 +0000

    Working model

--- Polygonal_Method.jl
+++ Polygonal_Method.jl
@@ -9,8 +9,8 @@ function Area(Arr; return_objects::Bool=false, return_graph::Bool=false)
     check_coincident!(circles)                              #check if any circles are coincident
     intersections = intersection_points(circles)            #get intersection points for all circles
     boundary_ID!(circles,intersections)                     #obtain boundary ID -> 1: on outer contour, 0: contained inside contour
-    polygons = form_polygons(circles,intersections)         #form polygons
-    sort_polygon_acw!(circles,polygons)                     #sort polygons acw
+    polygons, f, poly = form_polygons_graph(circles,intersections)         #form polygons
+    #sort_polygon_acw!(circles,polygons)                     #sort polygons acw
     contour = form_contour(circles)                         #form contour
     sectors = form_sectors(circles,intersections,contour)   #form sectors
 
@@ -109,11 +109,8 @@ function boundary_ID!(circles::Vector{Functions.Circle},intersections::Vector{Fu
 end
 
 function form_polygons_graph(circles, intersections)
-    # for each point in intersections, get points only on outer contour
     contour_points = [point for point in intersections if point.ID == 1]
 
-    # check if there are >1 contour points at the same x,y coordinate
-    # if so, combine them together into one point
     dirty = true
     while dirty
         dirty = false
@@ -122,115 +119,100 @@ function form_polygons_graph(circles, intersections)
             for j in i+1:length(contour_points)
                 point1 = contour_points[i]
                 point2 = contour_points[j]
-
+    
                 if isapprox(point1.x,point2.x,atol=0.1) && isapprox(point1.y,point2.y,atol=0.1)
-                    deleteat!(contour_points, [i,j]) # delete old points with same x,y coords formed from different circles
-                    new_circles = unique(vcat(point1.Circles, point2.Circles)) # get the circles that intersect on the point, no repeated circles
+                    deleteat!(contour_points, [i,j])
+                    new_circles = unique(vcat(point1.Circles, point2.Circles))
                     new_point = Functions.Point(point1.x, point1.y, new_circles, 1)
-                    push!(contour_points, new_point) # add this new point to our vector of contour points
+                    push!(contour_points, new_point)
                     dirty = true
                     super_break = true
                     break
                 end
             end
-
+    
             if super_break
                 break
             end
-
+    
         end
     end
 
-    # make a simple undirected graph using Graphs.jl
-    # nodes are contour points + every circle that make up those contour points
-    # (remember that a point is made up of 2 or more circles that intersect at that point)
-    # edges are connections between a point and the circles that make up that point
-
-    # get a vector of all the circles that make up the contour points, removing repeated circles
     tmp = [point.Circles for point in contour_points]
-    unique_circles = []
-    for i in 1:length(tmp)
-        for j in 1:length(tmp[i])
-            push!(unique_circles, tmp[i][j])
-        end
-    end
-
-    unique!(unique_circles)
-
+    graph_circles_index = reduce(vcat,tmp)
+    unique!(graph_circles_index)
+    graph_circles = circles[graph_circles_index]
+    
     # our vector of nodes
-    nodes = vcat(contour_points, circles[unique_circles])
-
+    nodes = vcat(contour_points, graph_circles)
+    
     g =SimpleGraph(length(nodes))
 
-    for i in 1:length(contour_points)
-        point_circle_index = contour_points[i].Circles # the circles that make up this contour point
-        for j in 1:length(unique_circles)
-            circle_index = unique_circles[j]
+    # for every circle -> edge between all intersecting circles and all contour points
+    for i in 1:length(graph_circles)
+        points = intersections[graph_circles[i].Points]
 
-            if circle_index in point_circle_index
-                add_edge!(g, i, j+length(contour_points)) # form edge
+        intersect!(points, contour_points)
+
+        index = Vector{Int}()
+        for p in points
+            dum = findall(x -> x == p, contour_points)
+
+            if length(dum) != 0
+                push!(index,dum[1]) 
             end
+        end
+
+        for j in index
+            add_edge!(g, i+length(contour_points), j)
+        end
+
+        index2 = []
+
+
+        my_circles = circles[graph_circles[i].Circles]
+
+        intersect!(my_circles,graph_circles)
+
+        index2 = Vector{Int}()
+        for c in my_circles
+            dum = findall(x -> x == c, graph_circles)
+
+            if length(dum) != 0
+                push!(index2,dum[1])
+            end
+        end
+
+        for j in index2
+            add_edge!(g, i+length(contour_points), j+length(contour_points))
         end
     end
 
     f = copy(g)
-    
-    special_nodes = []
-    for i in nv(g)-length(circles)+1:nv(g)
-        n_edges = degree(g,i)
-        if n_edges > 2 && n_edges %2 == 0
-            push!(special_nodes,i)
-        end
-    end
-    
-    poly = []
-    for i in 1:length(special_nodes)
-        for j in i+1:length(special_nodes)
-            node1 = special_nodes[i]
-            node2 = special_nodes[j]
-    
-            paths = yen_k_shortest_paths(g,node1,node2,weights(g),2).paths
-            if length(paths) == 0
-                continue
-            end
-            check = union(paths[1][2:end-1],paths[2][2:end-1])
-    
-            super_break = false
-            for k in 1:length(check)
-                if check[k] in special_nodes
-                    #println("Node ",node1," to node ",node2," is invalid")
-                    super_break = true
+
+    poly=[]
+    dirty = true
+    while dirty
+        dirty = false
+        for i in 1:length(contour_points)
+            d = degree(g,i)
+
+            if d == 2
+                n1,n2 = neighbors(g,i)
+                if has_edge(g,n1,n2) == true
+                    push!(poly,[i,n1,n2])
+                    rem_edge!(g,i,n1)
+                    rem_edge!(g,i,n2)
+                    dirty = true
                     break
                 end
             end
-    
-            if super_break
-                continue
-            end
-    
-            push!(poly,paths)
-    
-            for vertex in check
-                rem_edge!(g,node1,vertex)
-                rem_edge!(g,node2,vertex)
-            end
-    
         end
     end
-    
-    for i in 1:length(poly)
-        poly[i] = union(poly[i][1],poly[i][2])
-    end
-    
-    poly = vcat(poly,cycle_basis(g))
 
-    # get the minimal collection of cycles
-    # a cycle is essentially a polygon
-    #cycles = cycle_basis(g)
+    polygons = [nodes[i] for i in poly]
 
-    polygons = [nodes[i] for i in poly] #put the corresponding point and circle objects into polygons vector
-
-    return polygons, f
+    return polygons, f, poly
 end
 
 function form_polygons(circles::Vector{Functions.Circle},intersections::Vector{Functions.Point})
