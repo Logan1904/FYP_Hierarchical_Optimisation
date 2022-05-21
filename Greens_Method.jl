0f568e58b0ff16364c8feb00a7ecab2588639dff
Logan Sritharan <ls718@ic.ac.uk>
2022-05-21 18:01:26 +0100

    changed variable names

--- Greens_Method.jl
+++ Greens_Method.jl
@@ -4,11 +4,24 @@ export Greens_Problem
 
 using Base_Functions
 
+"""
+    struct Greens_Problem
+
+An object describing the problem of finding the area of the union of N intersecting circles
+The problem is solved by obtaining the outer contours of the circles, and calculating the area encompassed by Greens Theorem
+
+Attributes:
+    - 'circles::Vector{Circle}':                            A vector of Circle objects describing our problem
+    - 'intersections::Vector{Point}':                       A vector of Point objects describing the intersection points ONLY LOCATED ON AN OUTER CONTOUR
+    - 'contour::Vector{Vector{Circle}}':                    A vector of Circle objects, where each inner vector describes the Circle objects that are 'associated' with one another
+    - 'boundary::Vector{Vector{Vector{Vector{Any}}}}':      A vector of vector of vectors describing the outer boundaries
+    - 'area::Float64':                                      The calculated area
+"""
 struct Greens_Problem
     circles::Vector{Circle}
     intersections::Vector{Point}
-    contour::Vector{Vector{Circle}}
-    sectors::Vector{Vector{Vector{Any}}}
+    contours::Vector{Vector{Circle}}
+    boundaries::Vector{Vector{Vector{Any}}}
     area::Float64
 
     function Greens_Problem(x::Vector{Float64})
@@ -16,19 +29,28 @@ struct Greens_Problem
 
         original_circles = copy(circles)
 
-        circles = check_coincident!(circles)
-        circles = check_contained!(circles)
-        intersections = intersection_points(circles)
-        contour = form_contour(circles)
-        sectors = form_sectors(circles,intersections,contour)
-        area = calculateArea(sectors)
+        circles = checkCoincident!(circles)
+        circles = checkContained!(circles)
+        intersections = getIntersections(circles)
+        contours = getContours(circles)
+        boundaries = getBoundaries(circles,intersections,contours)
+        area = calculateArea(boundaries)
 
-        new(original_circles,intersections,contour,sectors,area)
+        new(original_circles,intersections,contours,boundaries,area)
     end
 
 end
 
-function check_coincident!(circles::Vector{Circle})
+"""
+    checkCoincident!(circles::Vector{Circle})
+
+Checks if any Circle objects are coincident with each other and removes the redundant Circle objects
+
+# Arguments:
+
+    - 'circles::Vector{Circle}': Vector of Circle objects
+"""
+function checkCoincident!(circles::Vector{Circle})
     clean = false
     while !clean
         N = Int(length(circles))
@@ -48,7 +70,16 @@ function check_coincident!(circles::Vector{Circle})
     return circles
 end
 
-function check_contained!(circles::Vector{Circle})
+"""
+    checkContained!(circles::Vector{Circle})
+
+Checks if any Circle objects are contained within one another and removes the contained Circle objects
+
+# Arguments:
+
+    - 'circles::Vector{Circle}': Vector of Circle objects
+"""
+function checkContained!(circles::Vector{Circle})
     for i in 1:length(circles)
         for j in i+1:length(circles)
             contained(circles[i],circles[j])
@@ -60,8 +91,21 @@ function check_contained!(circles::Vector{Circle})
     return new_circles
 end
 
+"""
+    getIntersections(circles::Vector{Circle})
 
-function intersection_points(circles::Vector{Circle})
+
+Obtains the intersection Point objects only located on an outer contour
+
+Updates the attributes of the Circle objects in 'circles' accordingly
+
+Returns intersections::Vector{Point} containing the Point objects
+
+# Arguments:
+
+    - 'circles::Vector{Circle}': Vector of Circle objects
+"""
+function getIntersections(circles::Vector{Circle})
     intersections = Vector{Point}()
     intersections_circles = []
     for i in 1:length(circles)
@@ -73,11 +117,13 @@ function intersection_points(circles::Vector{Circle})
             coords = intersection(circles[i],circles[j])
 
             if isnothing(coords) == false
+                # update the Circles attribute of the Circle objects
                 push!(A.Circles,i,j)
                 push!(B.Circles,i,j)
 
                 x1,y1,x2,y2 = coords
 
+                # form the Point objects representing the intersection points
                 P1 = Point(x1,y1,[i,j],1)
                 P2 = Point(x2,y2,[i,j],1)
 
@@ -94,6 +140,7 @@ function intersection_points(circles::Vector{Circle})
                     if contour_point
                         push!(intersections,P)
 
+                        # update the Points attribute of the Circle objects
                         dum = length(intersections)
                         push!(A.Points,dum)
                         push!(B.Points,dum)
@@ -105,6 +152,7 @@ function intersection_points(circles::Vector{Circle})
         end
     end
 
+    # Ensure no repetitions in the Circle attribute of the Circle objects
     for i in 1:length(circles)
         circles[i].Circles = unique(circles[i].Circles)
     end
@@ -112,42 +160,69 @@ function intersection_points(circles::Vector{Circle})
     return intersections
 end
 
-function form_contour(circles)# form circle associations
+"""
+    getContours(circles::Vector{Circle})
 
+Obtains the contours of our problem
+
+Returns contour::Vector{Vector{Circle}} where each inner vector contains the Circle objects that make up a contour
+
+# Arguments:
+
+    - 'circles::Vector{Circle}': Vector of Circle objects
+"""
+function getContours(circles)# form circle associations
+
+    # if there is just one Circle
     if length(circles) == 1
         return [circles]
     end
 
-    contour = []
-    contour_circles = [circ for circ in circles]
+    # form input format for associate function
+    contours = []
+    contour_circles = copy(circles)
     for i in range(1,stop=length(contour_circles))
         A = contour_circles[i]
-        push!(contour,[A.Circles,A])
+        push!(contours,[A.Circles,A])
     end
 
-    contour = associate(contour)
+    contours = associate(contours)
 
-    # change into vectors of circles
-    for i in range(1,stop=size(contour)[1])
-        contour[i] = [point[2] for point in contour[i]]
+    # change into vectors of Circles
+    for i in 1:length(contours)
+        contours[i] = [point[2] for point in contours[i]]
     end
 
-    return contour
+    return contours
 end
 
-function form_sectors(circles,intersections,contour)# form circular sectors
-    sectors = []
-    for i in range(1,stop=size(contour)[1])
-        contour_sectors = []
+"""
+    getBoundaries(circles::Vector{Circle}, intersections::Vector{Point}, contours::Vector{Vector{Circle}})
 
-        if size(contour[i])[1] == 1 # just the circle
-            push!(contour_sectors,[[contour[i][1],Point(0,0,[],0),Point(0,0,[],0),0.0,2*pi]])
-            push!(sectors,contour_sectors)
+
+Obtains the outer contours of our problem
+
+Returns contour::Vector{Vector{Circle}} where each inner vector contains the Circle objects that make up a contour
+
+# Arguments:
+
+    - 'circles::Vector{Circle}': Vector of Circle objects
+"""
+function getBoundaries(circles,intersections,contours)
+    boundaries = []
+    for i in range(1,stop=size(contours)[1])
+        contour_boundaries = []
+
+        # just the circle
+        if size(contours[i])[1] == 1 
+            push!(contour_boundaries,[[contours[i][1],Point(0,0,[],0),Point(0,0,[],0),0.0,2*pi]])
+            push!(boundaries,contour_boundaries)
             continue
         end
 
-        for j in range(1,stop=size(contour[i])[1]) # obtain circular sectors for each circle 
-            circle = contour[i][j]
+        # obtain the boundaries for each contour
+        for j in 1:length(contours[i]) 
+            circle = contours[i][j]
             boundary_points = intersections[circle.Points]
 
             sort_asc_angle!(circle,boundary_points)
@@ -166,11 +241,12 @@ function form_sectors(circles,intersections,contour)# form circular sectors
 
                 A = circles[circle.Circles]
 
-                for l in range(1,stop=length(A)) # check if point_check is inside any of the circles that intersect our current circle
+                # check if point_check is inside any of the circles that intersect our current circle
+                for l in range(1,stop=length(A)) 
                     if outside(A[l],point_check) == false
                         break
                     elseif l == length(A)
-                        push!(contour_sectors,[circle,boundary_points[k],boundary_points[next_iterator],theta[k],theta[next_iterator]])
+                        push!(contour_boundaries,[circle,boundary_points[k],boundary_points[next_iterator],theta[k],theta[next_iterator]])
                     end
                 end
 
@@ -178,73 +254,74 @@ function form_sectors(circles,intersections,contour)# form circular sectors
 
         end
 
-        for j in 1:length(contour_sectors)-1
-            end_sec1 = contour_sectors[j][3]
+        # sort the boundaries so they form a closed curve
+        for j in 1:length(contour_boundaries)-1
+            end_sec1 = contour_boundaries[j][3]
 
-            for k in j+1:length(contour_sectors)
-                start_sec2 = contour_sectors[k][2]
+            for k in j+1:length(contour_boundaries)
+                start_sec2 = contour_boundaries[k][2]
 
                 if end_sec1 == start_sec2
-                    tmp = contour_sectors[j+1]
-                    contour_sectors[j+1] = contour_sectors[k]
-                    contour_sectors[k] = tmp
+                    tmp = contour_boundaries[j+1]
+                    contour_boundaries[j+1] = contour_boundaries[k]
+                    contour_boundaries[k] = tmp
                     break
                 end
             end
         end
 
+        # split the boundaries so each boundary describes a single closed curve
         tmp = []
-        while length(contour_sectors) > 1
-            for j in 1:length(contour_sectors)-1
+        while length(contour_boundaries) > 1
+            for j in 1:length(contour_boundaries)-1
 
-                end_sec1 = contour_sectors[j][3]
-                start_sec2 = contour_sectors[j+1][2]
+                end_sec1 = contour_boundaries[j][3]
+                start_sec2 = contour_boundaries[j+1][2]
 
-                # check if there is a discontinuity in sectors
+                # check for discontinuity
                 if end_sec1 != start_sec2
-                    push!(tmp,contour_sectors[1:j])
-                    deleteat!(contour_sectors, 1:j)
+                    push!(tmp,contour_boundaries[1:j])
+                    deleteat!(contour_boundaries, 1:j)
                     break
                 end
 
-                if j == length(contour_sectors)-1
-                    push!(tmp,contour_sectors)
-                    contour_sectors = []
+                if j == length(contour_boundaries)-1
+                    push!(tmp,contour_boundaries)
+                    contour_boundaries = []
                 end
             end
         end
 
-        push!(sectors,tmp)
-
+        push!(boundaries,tmp)
     end
 
-    return sectors
+    return boundaries
 end
 
-function calculateArea(sectors)
+function calculateArea(boundaries)
 
     total_area = 0
 
     # for each collection
-    for i in 1:length(sectors)
+    for i in 1:length(boundaries)
 
         coll_area = []
 
         # for each contour
-        for j in 1:length(sectors[i])
+        for j in 1:length(boundaries[i])
             
-            if length(sectors[i][j]) == 1
-                push!(coll_area,pi*(sectors[i][1][1][1].R)^2)
+            if length(boundaries[i][j]) == 1
+                push!(coll_area,pi*(boundaries[i][1][1][1].R)^2)
                 break
             end
 
             vec = Vector{Point}()
 
-            for k in 1:length(sectors[i][j])
+            for k in 1:length(boundaries[i][j])
 
-                circle = sectors[i][j][k][1]
-                theta1 = sectors[i][j][k][4]
-                theta2 = sectors[i][j][k][5]
+                circle = boundaries[i][j][k][1]
+                theta1 = boundaries[i][j][k][4]
+                theta2 = boundaries[i][j][k][5]
 
                 if theta1 > theta2
                     theta2 = theta2 + 2*pi
