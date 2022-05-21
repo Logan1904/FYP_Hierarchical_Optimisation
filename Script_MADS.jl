878a867f23368f0cf160b41516f2fdf5bd70e38c
Logan Sritharan <ls718@ic.ac.uk>
2022-05-21 18:13:24 +0100

    Changes to reflect module changes

--- Script_MADS.jl
+++ Script_MADS.jl
@@ -1,13 +1,11 @@
-import Polygonal_Method, MonteCarlo_Method
-import Position_Optimization
-import Functions
-import Plotter
+using Greens_Method
+using Position_Optimization
 
 # Parameters
-N_drones = 8
+N = 8
 domain_x = 50
 domain_y = 50
-R_lim = 10
+R_lim = 7
 
 # MADS Parameters
 N_iter = 100
@@ -17,10 +15,11 @@ x_arr = Vector{Float64}(undef,0)
 y_arr = Vector{Float64}(undef,0)
 R_arr = Vector{Float64}(undef,0)
 
-for i in range(1,stop=N_drones)
+for i in 1:N
     x = rand(R_lim:domain_x-R_lim)[1]
     y = rand(R_lim:domain_y-R_lim)[1]
     R = rand(1:R_lim)[1]
+
 
     push!(x_arr,x)
     push!(y_arr,y)
@@ -29,16 +28,13 @@ end
 
 MADS_input = [x_arr;y_arr;R_arr]
 
-objective(z) = Polygonal_Method.Area(z)
-
 # Define objective
-function obj(z)
-    Area = Polygonal_Method.Area(z)
-    Distance = sqrt(sum((MADS_input - z).^2))
-    
-    println("Area = ",Area,", Distance = ",Distance,", Sum Minimized = ",-Area+Distance)
+function obj(x)
+    my_problem = Greens_Problem(x)
 
-    return -Area + Distance
+    distance = sqrt(sum((MADS_input - x).^2))
+    
+    return -my_problem.area + distance
 end
 
 # Define extreme and progressive constraints
@@ -50,9 +46,18 @@ cons_prog = []
 MADS_problem = Position_Optimization.define_problem(MADS_input, obj, cons_ext, cons_prog, N_iter)
 MADS_output = Position_Optimization.optimize(MADS_problem)
 
-# Print areas and plot images
-println("First iteration: ",objective(MADS_input))
-println("After optimization: ", objective(MADS_output))
+input_problem = Greens_Problem(MADS_input)
+output_problem = Greens_Problem(MADS_output)
 
-Plotter.plot_domain(Functions.make_circles(MADS_input),[domain_x,domain_y],"First_Iteration")
-Plotter.plot_domain(Functions.make_circles(MADS_output),[domain_x,domain_y],"Last_Iteration")
+# Plot input and output figures
+using Plotter
+
+Plot(input_problem,[domain_x,domain_y],"Input")
+Plot(output_problem,[domain_x,domain_y],"Output")
+
+# Print areas
+println("-----------INPUT----------")
+println("Greens Method: ",input_problem.area)
+println("")
+println("-----------OUTPUT----------")
+println("Greens Method: ",output_problem.area)
